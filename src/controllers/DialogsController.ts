1ce2949eb3fee0ab04e8c6a3208a51d244ef4024
Cryfear <vasil.arturok@yandex.ru>
2020-07-04 23:41:56 +0300

    connect redux

--- src/controllers/DialogsController.ts
+++ src/controllers/DialogsController.ts
@@ -17,21 +17,28 @@ let DialogsController = {
   createDialog: async (req: express.Request, res: express.Response) => {
     let user = await User.findOne({ _id: req.body.id_1 });
     let user2 = await User.findOne({ _id: req.body.id_2 });
-    if (user && user2)
-      new Dialog({
-        name: req.body.name,
-        users: [user._id, user2._id],
-      })
-        .populate("users")
-        .execPopulate()
-        .then(data => {
-          res.send(data);
-          data.save();
-          console.log(data);
+
+    if (user && user2) {
+      let dio = await Dialog.findOne({ users: [user._id, user2._id] });
+      if (dio === null) {
+        new Dialog({
+          name: req.body.name,
+          users: [user._id, user2._id],
         })
-        .catch(err => {
-          res.send(err);
-        });
+          .populate("users")
+          .execPopulate()
+          .then(data => {
+            res.send(data);
+            data.save();
+            console.log(data);
+          })
+          .catch(err => {
+            res.send(err);
+          });
+      } else {
+        res.send("такой диалог уже существует.");
+      }
+    }
   },
 
   updateDialog: async (req: express.Request, res: express.Response) => {
